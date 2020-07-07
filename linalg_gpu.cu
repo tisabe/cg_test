f7cd420374984790329386b3339b299a991db659
Tim Bechtel <tim.bechtel@physik.hu-berlin.de>
2020-07-07 16:40:30 +0200

    changed to scalars

--- linalg_gpu.cu
+++ linalg_gpu.cu
@@ -132,7 +132,7 @@ __global__ void dot_reduce(float *g_a, float *g_b, float *g_odata, unsigned int 
     if (tid == 0) g_odata[blockIdx.x] = sdata[0];
 }
 
-__global__ void reduceAdd (float *g_idata, float *g_odata, unsigned int n)
+__global__ void reduceAdd (float * res, float *g_idata, float *g_odata, unsigned int n)
 {
     // set thread ID
     unsigned int tid = threadIdx.x;
@@ -175,16 +175,22 @@ __global__ void reduceAdd (float *g_idata, float *g_odata, unsigned int n)
 
     // write result for this block to global mem
     //if (tid == 0) g_odata[blockIdx.x] = g_idata[idx];
-    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+    //if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+    if (tid == 0) {
+         *res = sdata[0];
+         printf("res = %f\n", *res);
+    }
 }
 
-void dot_wrapper(float *g_a, float *g_b, float *g_odata, unsigned int n) {
+void dot_wrapper(float *res, float *g_a, float *g_b, unsigned int n) {
     /* this function calculates the sum of elements in g_odata on the gpu,
     and saves it in the 0th element in g_idata. g_idata needs to have as many elements as there are threadblocks.
     It is assumed, that all arrays are already allocated on the gpu. 
     */
-    dot_reduce<<<TB_SIZE, TB_SIZE>>>(g_a, g_b, g_odata, n);
-    reduceAdd<<<1, TB_SIZE>>>(g_odata, g_odata, TB_SIZE);
+    float *tmp;
+    CHECK(cudaMalloc((void**) &tmp, gridsize*sizeof(float)));
+    dot_reduce<<<TB_SIZE, TB_SIZE>>>(g_a, g_b, tmp, n);
+    reduceAdd<<<1, TB_SIZE>>>(res, tmp, tmp, TB_SIZE);
 
 }
 
@@ -206,103 +212,105 @@ void mat_vec_mul_gpu(float *res, float *mat, float *v, int n) {
    mat_vec_mul_kernel<<<gridsize, blocksize>>>(res, mat, v, n);
 }
 
-__global__ void div_kernel(float *res, float *a, float *b, float s) {
+__global__ void div_kernel(float *res, float *a, float *b) {
    // res = s*a/b
 
    int idx=blockIdx.x*blockDim.x + threadIdx.x;
 
    if (idx == 0) {
-      res[0] = s*a[0]/b[0];
+      *res = (*a)/(*b);
    }
 }
 
-void div_gpu(float *res, float *a, float *b, float s) {
-   // res = s*a/b
-   div_kernel<<<1,1>>>(res, a, b, s);
+void div_gpu(float *res, float *a, float *b) {
+   // res = a/b
+   div_kernel<<<1,1>>>(res, a, b);
 }
 
-void cg_gpu(float * x, float * A, float * b, int n, int max_iter, float prec) {
-   float *r, *p, *ap; // vectors
+void scalar_cpy(float * dest, float * src) {
+   cudaMemcpy(dest, src, sizeof(float), cudaMemcpyDeviceToDevice); 
+}
+
+void cg_gpu(float * d_x, float * d_A, float * d_b, int n, int max_iter, float prec) {
+   float *d_r, *d_p, *d_ap; // vectors on device
    
-   CHECK(cudaMalloc((void**) &r, n*sizeof(float)));
-   CHECK(cudaMalloc((void**) &p, n*sizeof(float)));
-   CHECK(cudaMalloc((void**) &ap, n*sizeof(float)));
+   CHECK(cudaMalloc((void**) &d_r, n*sizeof(float)));
+   CHECK(cudaMalloc((void**) &d_p, n*sizeof(float)));
+   CHECK(cudaMalloc((void**) &d_ap, n*sizeof(float)));
 
-   float *alpha, *beta, *rr, *rr_n, *err, *pap; // scalars (vectors with size gridsize)
+   float *d_alpha, *d_beta, *d_rr, *d_rr_n, *d_err, *d_pap; // scalars on device
 
-   CHECK(cudaMalloc((void**) &alpha, gridsize*sizeof(float)));
-   CHECK(cudaMalloc((void**) &beta, gridsize*sizeof(float)));
-   CHECK(cudaMalloc((void**) &rr, gridsize*sizeof(float)));
-   CHECK(cudaMalloc((void**) &rr_n, gridsize*sizeof(float)));
-   CHECK(cudaMalloc((void**) &err, gridsize*sizeof(float)));
-   CHECK(cudaMalloc((void**) &pap, gridsize*sizeof(float)));
+   CHECK(cudaMalloc((void**) &d_alpha, sizeof(float)));
+   CHECK(cudaMalloc((void**) &d_beta, sizeof(float)));
+   CHECK(cudaMalloc((void**) &d_rr, sizeof(float)));
+   CHECK(cudaMalloc((void**) &d_rr_n, sizeof(float)));
+   CHECK(cudaMalloc((void**) &d_err, sizeof(float)));
+   CHECK(cudaMalloc((void**) &d_pap, sizeof(float)));
+
+   float h_err, h_err0; // scalars on host
    
    int k = 0;  // current iteration
 
    // value initialization
-   mat_vec_mul_gpu(p, A, x, n);  // p=A*x temp storage of A*x
-   sub_gpu(r, b, p, n); // r = b + -1*p
-   assign_v2v_gpu(p, r, n); // p = r
-   dot_wrapper(r, r, rr, n); 
-   assign_v2v_gpu(err, rr, gridsize);
+   mat_vec_mul_gpu(d_p, d_A, d_x, n);  // p=A*x temp storage of A*x
+   sub_gpu(d_r, d_b, d_p, n); // r = b + -1*p
+   assign_v2v_gpu(d_p, d_r, n); // p = r
+   dot_wrapper(d_rr, d_r, d_r, n); // rr = dot(r, r)
+   //assign_v2v_gpu(err, rr, gridsize);
+   scalar_cpy(d_err, d_rr);
+
+   CHECK(cudaMemcpy(&h_err0, d_err, sizeof(float), cudaMemcpyDeviceToHost));
+   h_err = h_err0;
+   h_err = 0;
+
+   printf("h_err0=%f\n", h_err0);
+   printf("h_err=%f\n", h_err);
+
    CHECK(cudaDeviceSynchronize());
    
-   float *h_err=(float*)malloc(gridsize*sizeof(float)); // error needs to be copied from device to host
-   CHECK(cudaMemcpy(h_err, err, gridsize*sizeof(float), cudaMemcpyDeviceToHost));
-   printf("h_err[0]=%f\n", h_err);
+   
 
-   float *err_0=(float*)malloc(gridsize*sizeof(float));
-   CHECK(cudaMemcpy(err_0, err, gridsize*sizeof(float), cudaMemcpyDeviceToHost));
-   printf("err_0[0]=%f\n", err_0);
-
-   while((k < max_iter) && (h_err[0] > prec*prec*err_0[0])) {
-      mat_vec_mul_gpu(ap, A, p, n);
-      dot_wrapper(r, ap, pap, n); // pap = dot(r, ap)
-      div_gpu(alpha, rr, pap, 1.0f);
+   while((k < max_iter) && (h_err > prec*prec*h_err0)) {
+      mat_vec_mul_gpu(d_ap, d_A, d_p, n);
+      dot_wrapper(d_pap, d_r, d_ap, n); // pap = dot(r, ap)
+      div_gpu(d_alpha, d_rr, d_pap); // alpha = rr/pap
       //scalar_vec_add(x, x, p, alpha, n);
-      mul_add_gpu(x, x, alpha, p, n);
+      mul_add_gpu(d_x, d_x, d_alpha, d_p, n);
       if (k != 0 && k%50 == 0) {
          //mat_vec_mul(ap, A, x, n);
-         mat_vec_mul_gpu(ap, A, x, n);
+         mat_vec_mul_gpu(d_ap, d_A, d_x, n);
          //scalar_vec_add(r, b, ap, -1.0f, n);
-         sub_gpu(r, b, ap, n);
+         sub_gpu(d_r, d_b, d_ap, n);
          printf("Residual adjusted!\n");
       } else {
          //scalar_vec_add(r, r, ap, -1.0f*alpha, n);
-         mul_sub_gpu(r, r, alpha, ap, n);
+         mul_sub_gpu(d_r, d_r, d_alpha, d_ap, n);
       }
       CHECK(cudaDeviceSynchronize());
       //rr_n = scalar_prod(r, r, n);
-      dot_wrapper(r, r, rr_n, n);
+      dot_wrapper(d_rr_n, d_r, d_r, n);
       //beta = rr_n/rr;
-      div_gpu(beta, rr_n, rr, 1.0f);
+      div_gpu(d_beta, d_rr_n, d_rr);
       //scalar_vec_add(p, r, p, beta, n);
-      mul_add_gpu(p, r, beta, p, n);
+      mul_add_gpu(d_p, d_r, d_beta, d_p, n);
       //rr = rr_n;
-      assign_v2v_gpu(rr, rr_n, gridsize);
+      scalar_cpy(d_rr, d_rr_n);
       //err = rr;
-      assign_v2v_gpu(err, rr, gridsize);
+      scalar_cpy(d_err, d_rr);
       
-      CHECK(cudaMemcpy(h_err, err, gridsize*sizeof(float), cudaMemcpyDeviceToHost));
+      //CHECK(cudaMemcpy(h_err, err, gridsize*sizeof(float), cudaMemcpyDeviceToHost));
+      CHECK(cudaMemcpyToSymbol(&h_err, d_err, sizeof(float), cudaMemcpyDeviceToHost));
 
       printf("Iteration: %d\n", k);
-      printf("Precision: %e\n", h_err[0]);
+      printf("Precision: %e\n", h_err);
 
       CHECK(cudaDeviceSynchronize());
 
       k++;
    }
    CHECK(cudaDeviceSynchronize());
-   CHECK(cudaFree(r));
-   CHECK(cudaFree(p));
-   CHECK(cudaFree(ap));
-   CHECK(cudaFree(alpha));
-   CHECK(cudaFree(beta));
-   CHECK(cudaFree(rr));
-   CHECK(cudaFree(rr_n));
-   CHECK(cudaFree(err));
-   CHECK(cudaFree(pap));
-   free(h_err);
-   free(err_0);
-
+   CHECK(cudaFree(d_r));
+   CHECK(cudaFree(d_p));
+   CHECK(cudaFree(d_ap));
+   
 }
